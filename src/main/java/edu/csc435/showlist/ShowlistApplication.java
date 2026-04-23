fbb8a75f78d5b57fc8f7271b54d3bfc22baedea3
hdavis602 <hdavis6@oswego.edu>
2026-04-23 13:12:32 -0400

    init commit

---
+++ src/main/java/edu/csc435/showlist/ShowlistApplication.java
@@ -0,0 +1,13 @@
+package edu.csc435.showlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/csc435/showlist/ShowlistApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.csc435.showlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
