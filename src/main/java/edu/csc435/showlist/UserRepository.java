396bb8cba96adc662eebc2e080bfbb4a6172a886
hdavis602 <hdavis6@oswego.edu>
2026-04-23 13:57:34 -0400

    added rest extension to UserRepository

--- src/main/java/edu/csc435/showlist/UserRepository.java
+++ src/main/java/edu/csc435/showlist/UserRepository.java
@@ -2,9 +2,9 @@ package edu.csc435.showlist;
 
 import java.util.*;
 import org.springframework.data.repository.*;
-//import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-public interface UserRepository extends CrudRepository<User, UUID> {
+public interface UserRepository extends PagingAndSortingRepository<User, UUID>, CrudRepository<User, UUID> {
     User findByUsername (String username);
     User findByUid(UUID uid);
 }
